57401811fb9e4298ffb3c6db00f03568b4a8cee2
kenvo08 <93984216+kenvo08@users.noreply.github.com>
2021-11-09 18:31:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Thân gửi (thân lắm mới gửi)",
+    introTitle: 'Đặng Lan Anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
